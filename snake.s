e2ccdcaf9c5476440b81e1ecc4864b373ac3c224
Nathan Flint <nlflint@gmail.com>
2019-06-20 11:29:43 -0400

    Created general function to clear row with given value

--- snake.s
+++ snake.s
@@ -4,6 +4,10 @@
 MAIN:
 JSR ENABLE_LOW_RES
 JSR CLEAR_SCREEN
+;JSR SHOW_TITLE
+;JSR CLEAR_SCREEN
+JSR DRAW_STATIC_GFX
+;JSR START_GAME
 JMP Done
 
 
@@ -14,8 +18,6 @@ ENABLE_LOW_RES:
  STA $C00C
  STA $C054
  RTS
-
-
 
 ;INITIALIZE VARIABLES
 CLEAR_SCREEN:
@@ -44,15 +46,36 @@ FillLoop2: STA (PixelPointer),Y
  CPX #8
  BNE FillRow
 
+
  ;clear last 4 lines of text
- LDX #20 ;row 21
- JSR ClearTextRow
- LDX #21 ;row 22
- JSR ClearTextRow
- LDX #22 ;row 23
- JSR ClearTextRow
- LDX #23 ;row 24
- JSR ClearTextRow
+ LDA #$20
+ ORA #%10000000
+ STA FILL_VALUE
+ 
+ LDA #20
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+
+ LDA #21
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+
+ LDA #22
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+
+ LDA #23
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+
+ ;LDA  ;row 21
+ ;JSR ClearTextRow
+ ;LDX #21 ;row 22
+ ;JSR ClearTextRow
+ ;LDX #22 ;row 23
+ ;JSR ClearTextRow
+ ;LDX #23 ;row 24
+ ;JSR ClearTextRow
  RTS
 
 ClearTextRow:
@@ -65,19 +88,64 @@ ClearTextRow:
  LDA #$20
  ORA #%10000000
 
- LDY #40
+ LDY #COLUMN_COUNT
 FillTextRowLoop:
  STA (PixelPointer),Y
  DEY
  BPL FillTextRowLoop
  RTS
 
+DRAW_STATIC_GFX:
+ JSR DRAW_GAME_BOX
+ ;JSR DRAW_SCORE_LABEL
+ RTS
 
+DRAW_GAME_BOX:
+ LDA #$FF
+ STA FILL_VALUE
+ LDA #00
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+ 
+ LDA #$DD
+ STA FILL_VALUE
+ LDA #19
+ STA FILL_ROW_NUMBER
+ JSR FILL_ROW
+ RTS
+
+COLUMN_COUNT = 39
+FILL_ROW_NUMBER: .byte $00
+FILL_VALUE: .byte $00
+FILL_ROW:
+ LDX FILL_ROW_NUMBER
+ LDA LO,X
+ STA PixelPointer
+ LDA HI,X
+ STA PixelPointer+1
+ LDA FILL_VALUE
+ LDY #COLUMN_COUNT
+FILL_LOOP:
+ STA (PixelPointer), Y
+ DEY
+ BPL FILL_LOOP
+ RTS
+
+ 
+ 
+TEXT_ADDR: .byte $00,$00
+DESTINATION_ADDR: .byte $00,$00
+COPY_TEXT_TO:
+ RTS
+ 
 
 Done:
 JMP Done
 
-ScoreText: .byte "Score"
+START_GAME:
+ ;
+
+ScoreText: .byte "Score",0
 
 ; Low-res square row map
 HI: .byte $04,$04,$05,$05,$06,$06,$07,$07
